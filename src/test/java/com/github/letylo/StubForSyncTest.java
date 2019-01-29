e4364399ae191f9aff2ff2731e26c7c9aa287c98
llopez <llopez@conwet.com>
2019-01-29 13:36:56 +0100

    Test the speed of synchronized stubFor

---
+++ src/test/java/com/github/letylo/StubForSyncTest.java
@@ -0,0 +1,133 @@
+package com.github.letylo;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
+import static org.assertj.core.api.BDDAssertions.then;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.github.tomakehurst.wiremock.stubbing.Scenario;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author leticia
+ */
+public class StubForSyncTest {
+
+    @Rule public WireMockRule wireMock = new WireMockRule(0);
+    
+    private ExecutorService executor;
+    private Client client;
+    private WebTarget target;
+    private String url;
+    private Queue<String> queue;
+    private CountDownLatch latch;
+    
+    @Before
+    public void setUp() {
+        
+        url = "http://localhost:" + wireMock.port();
+        client = ClientBuilder.newClient();
+        target = client.target(url);
+        queue = new ConcurrentLinkedDeque<>();
+        
+        wireMock.stubFor(get(urlMatching("/"))
+                .inScenario("Scenario")
+                .whenScenarioStateIs(Scenario.STARTED)
+                .willSetStateTo("FirstState")
+                .willReturn(
+                        WireMock.aResponse()
+                                .withBody("1")
+                                .withStatus(200)
+                                .withHeader("Content-Type", MediaType.TEXT_PLAIN)
+        ));
+
+        wireMock.stubFor(get(urlMatching("/"))
+                .inScenario("Scenario")
+                .whenScenarioStateIs("FirstState")
+                .willReturn(
+                        WireMock.aResponse()
+                                .withBody("2")
+                                .withStatus(200)
+                                .withHeader("Content-Type", MediaType.TEXT_PLAIN)
+        ));
+       
+        wireMock.start();
+
+        executor = Executors.newCachedThreadPool();   
+    }
+    
+    @After
+    public void tearDown() {
+        
+        executor.shutdown();
+    }
+     
+    private long shouldThrowMuchRequests() throws InterruptedException {
+
+        int size = 1000;
+        latch = new CountDownLatch(size);
+        
+        long start = System.nanoTime();
+        
+        for (int i = 0; i < size; i++) {
+            
+            executor.submit(() -> {
+                
+                try {
+                    String response = target
+                            .request(MediaType.TEXT_PLAIN)
+                            .get(String.class);
+                    
+//                    System.out.println(response);
+                    queue.add(response);
+                    
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        
+        long end = System.nanoTime();
+        long difference = end - start;
+        latch.await(10, TimeUnit.SECONDS);
+
+       
+        then(queue).containsOnlyOnce("1");
+        
+        return difference;
+    }
+    
+    @Test
+    public void runTwoHundredTimes() throws InterruptedException {
+        
+        long difference;
+        long timeToDoARequest;
+        
+        for (int i = 0; i < 200; i++) {
+            
+            difference = shouldThrowMuchRequests();
+            timeToDoARequest = TimeUnit.NANOSECONDS.toMicros(difference);
+            System.out.println(timeToDoARequest);
+        }       
+    }
+}
